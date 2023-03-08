025973e435a7cf5b44e9054e6e1c3621b542a2a7
Vyacheslav <slavamazur21@gmail.com>
2023-03-08 18:59:02 +0200

    reduced count of STUN server

--- src/views/room/components/RoomComponent.vue
+++ src/views/room/components/RoomComponent.vue
@@ -22,8 +22,6 @@ const iceServers = {
     { urls: "stun:stun1.l.google.com:19302" },
     { urls: "stun:stun2.l.google.com:19302" },
     { urls: "stun:stun3.l.google.com:19302" },
-    { urls: "stun:stun4.l.google.com:19302" },
-    { urls: "stun:stun.2talk.com:3478" },
   ],
 };
 
